72ca369580caaf0d4345186e555bfb4489cbb292
rosie6965 <ryujihyeon6965@gmail.com>
2017-06-27 11:17:52 +0900

    more

--- crawler.py
+++ crawler.py
@@ -1,10 +1,10 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 import csv
 
 loginURL = "https://db.kocca.kr/db/member/loginPage.do?menuNo=203303"
-listURL = "http://db.kocca.kr/db/broadcastdb/scriptList.do?menuNo=200462"
+listURL = "http://db.kocca.kr/db/broadcastdb/scriptList.do"
 driver = None
 
 ## chrome webdriver가 설치 되어 있어야 함
@@ -29,7 +29,6 @@ def login():
     driver.find_element_by_id("password").send_keys(profile[1])
     driver.find_element_by_xpath('//*[@name="loginForm"]/div/fieldset/div/span/a').click()
 
-
 def getDramaScript():
     # drama 대본 리스트
     global driver
@@ -37,11 +36,21 @@ def getDramaScript():
         login()
     driver.get(listURL)
     driver.implicitly_wait(3)
+    # 상세 검색 (드라마 선택)
+    driver.find_element_by_class_name("toggle-title").click()
+    driver.find_element_by_xpath('//label[@for="searchGenre0"]/input[@id="searchGenre0"]').click()
+    driver.find_element_by_xpath('//table/tbody/tr/th/a/img[@alt="상세검색"]').click()
+    # 드라마 목록 받아오기
     drama_links = [drama.get_attribute("href") for drama in driver.find_elements_by_xpath('//td[@class="left"]/a')]
-    # 메인 페이지 핸들러 저장
-    main_window_handle = None
-    while not main_window_handle:
-        main_window_handle = driver.current_window_handle
+    # paging
+    page = [index.get_attribute("href") for index in driver.find_elements_by_xpath('//div[@class="paging"]/a')]
+    for i in range(3, 11):
+        driver.get(page[i])
+        new_links = [drama.get_attribute("href") for drama in driver.find_elements_by_xpath('//td[@class="left"]/a')]
+        for link in new_links:
+            drama_links.append(link)
+
+    print("드라마 갯수: %d" % (len(drama_links)))
 
     for link in drama_links:
         driver.get(link)
@@ -52,7 +61,7 @@ def getDramaScript():
             drama_window_handle = driver.current_window_handle
 
         for btn in script_btn:
-            btn.click() # 자바스크립트라서 클릭해야 가져올 수 있음
+            btn.click()     # 자바스크립트라서 클릭해야 가져올 수 있음
             # 팝업창의 핸들러 저장
             signin_window_handle = None
             while not signin_window_handle:
@@ -61,9 +70,18 @@ def getDramaScript():
                         signin_window_handle = handle
                         break
             driver.switch_to.window(signin_window_handle)
-
-            print(driver.page_source)
+            outFile(driver.page_source)
             driver.close()
             driver.switch_to.window(drama_window_handle)
 
-        driver.switch_to.window(main_window_handle)
+    driver.close()
+    print("소스 파일 다운로드 완료!")
+
+
+def outFile(source):
+    bs = soup(source, 'html.parser')
+    title = bs.find(id="title")
+    f = open("./" + title + ".txt", 'wt')
+    f.write(bs.string)
+    f.close()
+
